a06699f1dfa7b5485f67501826e46410c89a2364
Peter Choi <phc284@gmail.com>
2018-03-04 17:22:07 -0600

    footer internet color change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ import InternetTabImg from '../Cuts/tab_internets@2x.png'
 const Footer = () => {
     return (
       <div className="footer">
-	      <div className="footer-tab">
+	      <div className="footer-tab lunch">
 	      	<img src={LunchTabImg} alt=""/>
 	      	<span>lunch</span>
 	      </div>
-	      <div className="footer-tab">
+	      <div className="footer-tab internets">
 	      	<img src={InternetTabImg} alt=""/>
 	      	<span>internets</span>
 	      </div>
